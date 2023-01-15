543fca7a80f47dd8aafa6e087a4350bf3875abd1
Baptiste Langlade <baptouuuu@gmail.com>
2023-01-15 18:09:58 +0100

    add a time frame continuation strategy

---
+++ src/Continuation/TimeFrame.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Mantle\Continuation;
+
+use Innmind\TimeContinuum\{
+    Clock,
+    ElapsedPeriod,
+    PointInTime,
+};
+
+final class TimeFrame implements Strategy
+{
+    private Clock $clock;
+    private ElapsedPeriod $frame;
+    private PointInTime $lastSuspended;
+
+    private function __construct(Clock $clock, ElapsedPeriod $frame)
+    {
+        $this->clock = $clock;
+        $this->frame = $frame;
+        $this->lastSuspended = $clock->now();
+    }
+
+    public function __invoke(): bool
+    {
+        $shouldSuspend = $this
+            ->clock
+            ->now()
+            ->elapsedSince($this->lastSuspended)
+            ->longerThan($this->frame);
+
+        if ($shouldSuspend) {
+            $this->lastSuspended = $this->clock->now();
+        }
+
+        return $shouldSuspend;
+    }
+
+    /**
+     * @return callable(): Strategy
+     */
+    public static function of(Clock $clock, ElapsedPeriod $frame): callable
+    {
+        return static fn() => new self($clock, $frame);
+    }
+}
---
+++ tests/Continuation/TimeFrameTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Mantle\Continuation;
+
+use Innmind\Mantle\{
+    Continuation\TimeFrame,
+    Continuation\Strategy,
+};
+use Innmind\TimeContinuum\{
+    Earth\Clock,
+    Earth\ElapsedPeriod,
+};
+use PHPUnit\Framework\TestCase;
+use Innmind\BlackBox\{
+    PHPUnit\BlackBox,
+    Set,
+};
+
+class TimeFrameTest extends TestCase
+{
+    use BlackBox;
+
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Strategy::class,
+            TimeFrame::of(new Clock, ElapsedPeriod::of(1))(),
+        );
+    }
+
+    public function testShouldSuspend()
+    {
+        $this
+            ->forAll(Set\Integers::between(1, 450000))
+            ->then(function($microseconds) {
+                $shouldSuspend = TimeFrame::of(new Clock, ElapsedPeriod::of(500))();
+
+                $this->assertFalse($shouldSuspend());
+                \usleep($microseconds);
+                $this->assertFalse($shouldSuspend());
+            });
+
+        $shouldSuspend = TimeFrame::of(new Clock, ElapsedPeriod::of(500))();
+
+        \usleep(501000);
+        $this->assertTrue($shouldSuspend());
+    }
+}
